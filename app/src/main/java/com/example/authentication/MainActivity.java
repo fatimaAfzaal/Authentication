637521b42d3a2f6e2c8f79b8f7b140527f8f990f
fatimaAfzaal <fatimaafzaal2002@gmail.com>
2022-04-28 06:24:13 -0800

    Initial commit

---
+++ app/src/main/java/com/example/authentication/MainActivity.java
@@ -0,0 +1,40 @@
+package com.example.authentication;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btnLogOut;
+    FirebaseAuth mAuth;
+
+    protected void onCreate(Bundle savedInstanceState) {
+        getSupportActionBar().hide();
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnLogOut = findViewById(R.id.btnLogout);
+        mAuth = FirebaseAuth.getInstance();
+
+        btnLogOut.setOnClickListener(view ->{
+            mAuth.signOut();
+            startActivity(new Intent(MainActivity.this, loginActivity.class));
+        });
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        FirebaseUser user = mAuth.getCurrentUser();
+        if (user == null){
+            startActivity(new Intent(MainActivity.this, loginActivity.class));
+        }
+    }
+}
